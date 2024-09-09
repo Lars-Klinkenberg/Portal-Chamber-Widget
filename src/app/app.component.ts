b43f00e05755f4cff46580080b0c6cfa12f30b00
Lars-Klinkenberg <klinkenberg.lars@gmail.com>
2024-09-09 02:39:59 +0200

    chore: Update styles and dependencies
    - Update global styles to set width and height for html, body, and section elements
    - Import CommonModule in app.component.ts
    - Add date and progress properties to AppComponent class in app.component.ts
    - Add font link in index.html
    - Add assets folder to angular.json configurations
    - Update styles in app.component.scss to improve layout and alignment

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,22 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [CommonModule, RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'Portal-Chamber-Widget';
+  day: Date = new Date();
+  progress = 15;
+
+  constructor() {
+    setInterval(() => {
+      this.day = new Date();
+    }, 100);
+  }
 }
